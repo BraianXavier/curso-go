348d7a33a0d3f0c32f1afbb1930b51ebdbf33932
braian <braianf.xavier@gmail.com>
2019-02-06 14:24:32 -0200

    buffer

--- concorrencia/buffer/buffer.go
+++ concorrencia/buffer/buffer.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func rotina(ch chan int) {
+	fmt.Println("Executou!")
+	ch <- 1
+	ch <- 2
+	ch <- 3
+	ch <- 4
+	ch <- 5
+	ch <- 6
+
+}
+func main() {
+	ch := make(chan int, 3)
+	go rotina(ch)
+
+	time.Sleep(time.Second)
+	fmt.Println(<-ch)
 }
